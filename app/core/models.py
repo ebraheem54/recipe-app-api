5f9bcbf56d171e7981389b0f50d0f2595db88527
ebraheem54 <ebraheem.sy370@gmail.com>
2026-03-24 17:04:18 +0300

    add normaliz to email in model and test for this feature

--- app/core/models.py
+++ app/core/models.py
@@ -12,9 +12,9 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
   """Manager for users."""
 
-  def create_user(self, email, password=None, **extra_filed):
+  def create_user(self, email, password=None, **extra_fields):
     """Create, save and return a new user."""
-    user = self.model(email=email,  **extra_filed)
+    user = self.model(email=self.normalize_email(email),  **extra_fields)
     user.set_password(password)
     user.save(using=self._db)
 
@@ -23,11 +23,11 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
   """User in the system."""
-  email=models.EmailField(max_length=255,unique=True)
-  name=models.CharField(max_length=255)
-  is_active=models.BooleanField(default=True)
-  is_staff=models.models.BooleanField(default=False)
+  email = models.EmailField(max_length=255,unique=True)
+  name = models.CharField(max_length=255)
+  is_active = models.BooleanField(default=True)
+  is_staff = models.BooleanField(default=False)
 
   objects = UserManager()
 
-  USERNAME_FILED = 'email'
+  USERNAME_FIELD = 'email'
