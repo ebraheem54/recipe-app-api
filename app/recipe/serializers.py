9d9581072bf08ca9db1514e6b41eee11840a6a50
ebraheem54 <ebraheem.sy370@gmail.com>
2026-04-16 04:21:18 +0300

    create recipe app and test for it and model recipe

---
+++ app/recipe/serializers.py
@@ -0,0 +1,12 @@
+"""
+Serializers for recipe APIS
+"""
+from rest_framework import serializers
+from core.models import Recipe
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """serializer for recipes."""
+    class Meta:
+        model=Recipe
+        fields=['id','title','time_minutes','price','link','description']
+        read_only_fields=['id']
--- app/user/views.py
+++ app/user/views.py
@@ -1,13 +1,8 @@
-from django.shortcuts import render
-
-from rest_framework import generics, authentication ,permissions
+from rest_framework import generics, authentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from rest_framework.permissions import IsAuthenticated
-from user.serializers import (
-    UserSerializer,
-    AuthTokenSerializer
-)
+from user.serializers import UserSerializer, AuthTokenSerializer
 
 
 # Create your views here.
@@ -18,17 +13,19 @@ class CreateUserView(generics.CreateAPIView):
 
 
 class CreateTokenView(ObtainAuthToken):
-    """Create a new auth token for user """
+    """Create a new auth token for user"""
+
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class ManageUserView(generics.RetrieveUpdateAPIView):
     """Manage the authenticate user"""
+
     serializer_class = UserSerializer
     authentication_classes = [authentication.TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
-        """Retrieve and return the authenticated user """
+        """Retrieve and return the authenticated user"""
         return self.request.user
