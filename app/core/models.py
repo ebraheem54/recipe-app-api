3b2c366897fc5e4ae4eb74b82d7ed795efe5434b
ebraheem54 <ebraheem.sy370@gmail.com>
2026-04-30 07:03:37 +0300

    write testTag and viewset mixind

--- app/core/models.py
+++ app/core/models.py
@@ -37,6 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
 
     email = models.EmailField(max_length=255, unique=True)
+
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -56,6 +57,17 @@ class Recipe(models.Model):
     time_minutes = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
-
+    tag=models.ManyToManyField('Tag')
     def __str__(self):
         return self.title
+
+class Tag(models.Model):
+    """Tag for filtering Recipes """
+    name =models.CharField(max_length=255)
+    user=models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
